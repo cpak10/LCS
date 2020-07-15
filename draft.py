ba97f8397717c05e83f3d2aa6e1a8712ba2d19fc
Chris Pak <christopher.a.pak@gmail.com>
2020-07-14 22:34:08 -0700

    updated with percent win across game time

--- draft.py
+++ draft.py
@@ -1,169 +1,170 @@
-import pandas as pd
+from selenium import webdriver
+import re
+import time
 import matplotlib.pylab as plt
+from scipy.interpolate import interp1d
+import numpy as np
+
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(chrome_options=options)
 
 # input first team
-first_team = input('Enter first team here: ')
-first_1 = input('Enter champion here: ')
-first_2 = input('Enter champion here: ')
-first_3 = input('Enter champion here: ')
-first_4 = input('Enter champion here: ')
-first_5 = input('Enter champion here: ')
+team_name1 = input('Enter first team name here: ')
+first_1 = input('Enter top champion here: ')
+first_2 = input('Enter jungle champion here: ')
+first_3 = input('Enter mid champion here: ')
+first_4 = input('Enter adc champion here: ')
+first_5 = input('Enter support champion here: ')
 
 # input second team
-second_team = input('Enter second team here: ')
-second_1 = input('Enter champion here: ')
-second_2 = input('Enter champion here: ')
-second_3 = input('Enter champion here: ')
-second_4 = input('Enter champion here: ')
-second_5 = input('Enter champion here: ')
+team_name2 = input('Enter second team name here: ')
+second_1 = input('Enter top champion here: ')
+second_2 = input('Enter jungle champion here: ')
+second_3 = input('Enter mid champion here: ')
+second_4 = input('Enter adc champion here: ')
+second_5 = input('Enter support champion here: ')
 
-# lists
-first_team_lst = [first_1, first_2, first_3, first_4, first_5]
-second_team_lst = [second_1, second_2, second_3, second_4, second_5]
+# dictionaries
+first_team = {
+    0: first_1,
+    1: first_2,
+    2: first_3,
+    3: first_4,
+    4: first_5
+}
 
-# team dictionaries does math
+second_team = {
+    0: second_1,
+    1: second_2,
+    2: second_3,
+    3: second_4,
+    4: second_5
+}
+
+positions = {
+    0: 'Top',
+    1: 'Jungle',
+    2: 'Middle',
+    3: 'ADC',
+    4: 'Support'
+}
+
 team1_dict = {
-            15: 0, 16: 0, 17: 0, 18: 0, 19: 0, 20: 0,
-            21: 0, 22: 0, 23: 0, 24: 0, 25: 0, 26: 0,
-            27: 0, 28: 0, 29: 0, 30: 0, 31: 0, 32: 0,
-            33: 0, 34: 0, 35: 0, 36: 0, 37: 0, 38: 0,
-            39: 0, 40: 0, 41: 0, 42: 0, 43: 0, 44: 0,
-            45: 0, 46: 0, 47: 0, 48: 0, 49: 0, 50: 0,
-            51: 0, 52: 0, 53: 0, 54: 0, 55: 0, 56: 0,
-            57: 0, 58: 0, 59: 0, 60: 0, 61: 0, 62: 0,
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0
 }
+
 team2_dict = {
-            15: 0, 16: 0, 17: 0, 18: 0, 19: 0, 20: 0,
-            21: 0, 22: 0, 23: 0, 24: 0, 25: 0, 26: 0,
-            27: 0, 28: 0, 29: 0, 30: 0, 31: 0, 32: 0,
-            33: 0, 34: 0, 35: 0, 36: 0, 37: 0, 38: 0,
-            39: 0, 40: 0, 41: 0, 42: 0, 43: 0, 44: 0,
-            45: 0, 46: 0, 47: 0, 48: 0, 49: 0, 50: 0,
-            51: 0, 52: 0, 53: 0, 54: 0, 55: 0, 56: 0,
-            57: 0, 58: 0, 59: 0, 60: 0, 61: 0, 62: 0,
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0
 }
 
-# calculator
-def team_avg_time(close_time, team):
-    if close_time == 0:
-        return
-    if team == first_team:
-        for i in range(-9, 10):
-            time_var = close_time + i
-            var = team1_dict.get(time_var)
-            team1_dict[time_var] = var + (100 - (abs(i) * 10))
-    if team == second_team:
-        for i in range(-9, 10):
-            time_var = close_time + i
-            var = team2_dict.get(time_var)
-            team2_dict[time_var] = var + (100 - (abs(i) * 10))
-    else:
-        pass
+time_dict = {
+    0: 20,
+    1: 25,
+    2: 30,
+    3: 40,
+    4: 50
+}
 
-# odds calculator
-def final_calc(time):
-    team1 = team1_dict.get(time)
-    team2 = team2_dict.get(time)
-    total = team1 + team2
-    if total == 0:
-        return 'Almost no chance of either team winning at ' + str(time) + ' minutes.'
-    else:
-        team1_chance = round(team1/total * 100, 2)
-        team2_chance = round(team2/total * 100, 2)
-    return 'Chances at ' + str(time) + ' minutes for ' + first_team + ' to win is ' + str(team1_chance) + '%, ' + second_team + ' is ' + str(team2_chance) + '%.'
+# calculating chance
+def final_calc(time_index):
+    timex = time_dict.get(time_index)
+    total = team1_dict.get(time_index) + team2_dict.get(time_index)
+    team1_chance = round(team1_dict.get(time_index)/total * 100, 2)
+    team2_chance = round(team2_dict.get(time_index)/total * 100, 2)
+    return 'Chances at ' + str(timex) + ' minutes for ' + team_name1 + ' to win is ' + str(team1_chance) + '%, ' + team_name2 + ' is ' + str(team2_chance) + '%.'
 
-# read teams
-team_stats = pd.read_excel('team_stats.xlsx')
-team1_stats = team_stats.loc[team_stats['Name'] == first_team]
-close_time = int(team1_stats['Game Duration'])
-print('Average time for ' + first_team + ' to close out a game: ' + str(close_time))
-team_avg_time(close_time, first_team)
-team2_stats = team_stats.loc[team_stats['Name'] == second_team]
-close_time2 = int(team2_stats['Game Duration'])
-print('Average time for ' + second_team + ' to close out a game: ' + str(close_time2))
-team_avg_time(close_time2, second_team)
+# web scraping
+for i in range(0, 5):
+    champ = first_team.get(i)
+    champ_position = positions.get(i)
+    driver.get('https://champion.gg/champion/' + champ + '/' + champ_position)
+    time.sleep(1)
+    html = driver.page_source
+    time.sleep(1)
+    game_length = html.find('\"gameLength\"')
+    window = html[game_length:game_length+300]
+    first_bracket = window.find('[')
+    close_bracket = window.find(']') + 1
+    stats_str = window[first_bracket:close_bracket]
+    stats_str_replace = stats_str.replace('\"', ' ', 10)
+    stats = re.findall(r'[\d\.\d]+', stats_str_replace)
+    stats_int = [float(i) for i in stats]
+    for i in range(len(stats_int)):
+        team1_dict[i] = (stats_int[i]/100)**10 + team1_dict[i]
 
-# read champions first team
-champ_stats = pd.read_excel('champion_stats.xlsx')
-for i in first_team_lst:
-    champ = champ_stats.loc[champ_stats['Champion'] == (i + ' ' + i)]
-    champ_close = int(champ['GTR'])
-    team_avg_time(champ_close, first_team)
+for i in range(0, 5):
+    champ = second_team.get(i)
+    champ_position = positions.get(i)
+    driver.get('https://champion.gg/champion/' + champ + '/' + champ_position)
+    time.sleep(1)
+    html = driver.page_source
+    time.sleep(1)
+    game_length = html.find('\"gameLength\"')
+    window = html[game_length:game_length+300]
+    first_bracket = window.find('[')
+    close_bracket = window.find(']') + 1
+    stats_str = window[first_bracket:close_bracket]
+    stats_str_replace = stats_str.replace('\"', ' ', 10)
+    stats = re.findall(r'[\d\.\d]+', stats_str_replace)
+    stats_int = [float(i) for i in stats]
+    for i in range(len(stats_int)):
+        team2_dict[i] = (stats_int[i]/100)**10 + team2_dict[i]
 
-# read champions second team
-for i in second_team_lst:
-    champ = champ_stats.loc[champ_stats['Champion'] == (i + ' ' + i)]
-    champ_close = int(champ['GTR'])
-    team_avg_time(champ_close, second_team)
+for i in range (0, 5):
+    print(final_calc(i))
 
-# outputs
-print(final_calc(20))
-print(final_calc(25))
-print(final_calc(30))
-print(final_calc(35))
-print(final_calc(40))
-print(final_calc(45))
-print(final_calc(50))
-print(final_calc(55))
-print(final_calc(close_time))
-print(final_calc(close_time2))
-
-# graph
+# plotting 
 team1_dict1 = {
-            15: 'nan', 16: 'nan', 17: 'nan', 18: 'nan', 19: 'nan', 20: 'nan',
-            21: 'nan', 22: 'nan', 23: 'nan', 24: 'nan', 25: 'nan', 26: 'nan',
-            27: 'nan', 28: 'nan', 29: 'nan', 30: 'nan', 31: 'nan', 32: 'nan',
-            33: 'nan', 34: 'nan', 35: 'nan', 36: 'nan', 37: 'nan', 38: 'nan',
-            39: 'nan', 40: 'nan', 41: 'nan', 42: 'nan', 43: 'nan', 44: 'nan',
-            45: 'nan', 46: 'nan', 47: 'nan', 48: 'nan', 49: 'nan', 50: 'nan',
-            51: 'nan', 52: 'nan', 53: 'nan', 54: 'nan', 55: 'nan', 56: 'nan',
-            57: 'nan', 58: 'nan', 59: 'nan', 60: 'nan', 61: 'nan', 62: 'nan',
-}
-team2_dict1 = {
-            15: 'nan', 16: 'nan', 17: 'nan', 18: 'nan', 19: 'nan', 20: 'nan',
-            21: 'nan', 22: 'nan', 23: 'nan', 24: 'nan', 25: 'nan', 26: 'nan',
-            27: 'nan', 28: 'nan', 29: 'nan', 30: 'nan', 31: 'nan', 32: 'nan',
-            33: 'nan', 34: 'nan', 35: 'nan', 36: 'nan', 37: 'nan', 38: 'nan',
-            39: 'nan', 40: 'nan', 41: 'nan', 42: 'nan', 43: 'nan', 44: 'nan',
-            45: 'nan', 46: 'nan', 47: 'nan', 48: 'nan', 49: 'nan', 50: 'nan',
-            51: 'nan', 52: 'nan', 53: 'nan', 54: 'nan', 55: 'nan', 56: 'nan',
-            57: 'nan', 58: 'nan', 59: 'nan', 60: 'nan', 61: 'nan', 62: 'nan',
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0
 }
 
-def final_calc1(time):
-    team1 = team1_dict.get(time)
-    team2 = team2_dict.get(time)
-    total = team1 + team2
-    if total == 0:
-        return 0
-    else:
-        team1_chance = round(team1/total * 100, 2)
+team2_dict1 = {
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0
+}
+
+def final_calc1(time_index):
+    timex = time_dict.get(time_index)
+    total = team1_dict.get(time_index) + team2_dict.get(time_index)
+    team1_chance = round(team1_dict.get(time_index)/total * 100, 2)
     return team1_chance
 
-def final_calc2(time):
-    team1 = team1_dict.get(time)
-    team2 = team2_dict.get(time)
-    total = team1 + team2
-    if total == 0:
-        return 0
-    else:
-        team2_chance = round(team2/total * 100, 2)
+def final_calc2(time_index):
+    timex = time_dict.get(time_index)
+    total = team1_dict.get(time_index) + team2_dict.get(time_index)
+    team2_chance = round(team2_dict.get(time_index)/total * 100, 2)
     return team2_chance
 
-for i in range(15, 63):
+for i in range(0, 5):
     team1_dict1[i] = final_calc1(i)
     team2_dict1[i] = final_calc2(i)
-    if team1_dict1[i] == 0 or team1_dict1[i] == 100:
-        team1_dict1.pop(i)
-    if team2_dict1[i] == 0 or team2_dict1[i] == 100:
-        team2_dict1.pop(i)
 
 lists = sorted(team1_dict1.items())
-x, y = zip(*lists)
+x1, y1 = zip(*lists)
 lists2 = sorted(team2_dict1.items())
-x1, y1 = zip(*lists2)
-plt.plot(x, y, label=first_team)
-plt.plot(x1, y1, label=second_team)
+x2, y2 = zip(*lists2)
+f1 = interp1d(x1, y1, kind='cubic')
+f2 = interp1d(x2, y2, kind='cubic')
+xnew1 = np.linspace(0, 4, 300)
+xnew2 = np.linspace(0, 4, 300)
+plt.plot(xnew1, f1(xnew1), label=team_name1)
+plt.plot(xnew2, f2(xnew2), label=team_name2)
+plt.xticks((0, 1, 2, 3, 4), ('20', '25', '30', '40', '50'))
 plt.xlabel('minutes')
 plt.ylabel('percent chance of win')
 plt.legend()
